ce9950b538c227310f0b1d574375e4abac114240
Fabian <heinzefab@gmail.com>
2021-02-18 22:12:15 +0100

    Projektdateien hinzufügen.

---
+++ ExtraRolesUpdater/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace ExtraRolesUpdater
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.CurrentVersionLabel = new System.Windows.Forms.Label();
+            this.CheckUpdateButton = new System.Windows.Forms.Button();
+            this.selectDirectory = new System.Windows.Forms.Button();
+            this.DirectoryLabel = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.DeleteModButton = new System.Windows.Forms.Button();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CurrentVersionLabel
+            // 
+            this.CurrentVersionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentVersionLabel.Location = new System.Drawing.Point(12, 215);
+            this.CurrentVersionLabel.Name = "CurrentVersionLabel";
+            this.CurrentVersionLabel.Size = new System.Drawing.Size(445, 30);
+            this.CurrentVersionLabel.TabIndex = 0;
+            this.CurrentVersionLabel.Text = "Installed ExtraRoles Mod Version: None";
+            this.CurrentVersionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CheckUpdateButton
+            // 
+            this.CheckUpdateButton.Enabled = false;
+            this.CheckUpdateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckUpdateButton.Location = new System.Drawing.Point(12, 361);
+            this.CheckUpdateButton.Name = "CheckUpdateButton";
+            this.CheckUpdateButton.Size = new System.Drawing.Size(445, 31);
+            this.CheckUpdateButton.TabIndex = 2;
+            this.CheckUpdateButton.Text = "Check for Update";
+            this.CheckUpdateButton.UseVisualStyleBackColor = true;
+            this.CheckUpdateButton.Click += new System.EventHandler(this.CheckUpdateButton_Click);
+            // 
+            // selectDirectory
+            // 
+            this.selectDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectDirectory.Location = new System.Drawing.Point(12, 287);
+            this.selectDirectory.Name = "selectDirectory";
+            this.selectDirectory.Size = new System.Drawing.Size(445, 31);
+            this.selectDirectory.TabIndex = 3;
+            this.selectDirectory.Text = "Select Among Us Directory";
+            this.selectDirectory.UseVisualStyleBackColor = true;
+            this.selectDirectory.Click += new System.EventHandler(this.SelectDirectory_Click);
+            // 
+            // DirectoryLabel
+            // 
+            this.DirectoryLabel.AutoSize = true;
+            this.DirectoryLabel.Location = new System.Drawing.Point(9, 261);
+            this.DirectoryLabel.Name = "DirectoryLabel";
+            this.DirectoryLabel.Size = new System.Drawing.Size(167, 13);
+            this.DirectoryLabel.TabIndex = 4;
+            this.DirectoryLabel.Text = "Among Us Directory: Not selected";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(469, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = false;
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(300, 17);
+            this.StatusLabel.Text = "Status:";
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Location = new System.Drawing.Point(9, 398);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(197, 23);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Mod Manager by HORST";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelClicked);
+            // 
+            // DeleteModButton
+            // 
+            this.DeleteModButton.Enabled = false;
+            this.DeleteModButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteModButton.Location = new System.Drawing.Point(12, 324);
+            this.DeleteModButton.Name = "DeleteModButton";
+            this.DeleteModButton.Size = new System.Drawing.Size(445, 31);
+            this.DeleteModButton.TabIndex = 7;
+            this.DeleteModButton.Text = "Delete ExtraRoles Mod";
+            this.DeleteModButton.UseVisualStyleBackColor = true;
+            this.DeleteModButton.Click += new System.EventHandler(this.DeleteModClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.Location = new System.Drawing.Point(260, 398);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(197, 23);
+            this.linkLabel2.TabIndex = 10;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "ExtraRoles Mod by NotHunter101";
+            this.linkLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.ModLinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ExtraRolesUpdater.Properties.Resources.characterGraphic1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(445, 200);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 450);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.DeleteModButton);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.DirectoryLabel);
+            this.Controls.Add(this.selectDirectory);
+            this.Controls.Add(this.CheckUpdateButton);
+            this.Controls.Add(this.CurrentVersionLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ExtraRoles Mod Manager";
+            this.Load += new System.EventHandler(this.OnLoad);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CurrentVersionLabel;
+        private System.Windows.Forms.Button CheckUpdateButton;
+        private System.Windows.Forms.Button selectDirectory;
+        private System.Windows.Forms.Label DirectoryLabel;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button DeleteModButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
